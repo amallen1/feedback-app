00ba3c15761e9dcdb35e1085734bcc1565ae851a
amallen1 <amallen046@gmail.com>
2022-04-04 09:16:01 -0500

    update

--- client/src/services/suggestions.js
+++ client/src/services/suggestions.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const suggestionApi = createApi({
   reducerPath: "suggestionApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://productfeedback-app.herokuapp.com/api",
   }),
   tagTypes: ["Suggestions"],
   endpoints: (builder) => ({
--- server/index.js
+++ server/index.js
@@ -8,11 +8,12 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.DB_URL);
-
-mongoose.connection.once("open", () => {
-  console.log("Database connected");
-});
+mongoose
+  .connect(process.env.DB_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((error) => console.log(error));
 
 const userRoutes = require("./routes/user");
 const feedbackRoutes = require("./routes/feedbackRoute");
