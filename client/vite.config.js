12f41cd7e1d7c3f57956afa91686d5a9b029248c
amallen1 <amallen046@gmail.com>
2025-01-03 20:32:37 -0600

    fixed issues with doppler and env variables

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import macrosPlugin from "vite-plugin-babel-macros";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), macrosPlugin()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), macrosPlugin()],
+    define: {
+      "process.env.API_BASE_URL": JSON.stringify(env.API_BASE_URL),
+    },
+  };
 });
