3aa47d568dca8ab9a882005c7d00e66b047ee552
amallen1 <amallen046@gmail.com>
2025-01-03 19:47:39 -0600

    fixed UI issues after vite migration

--- client/src/services/feedbacks.js
+++ client/src/services/feedbacks.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const feedbackApi = createApi({
   reducerPath: "feedbackApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.REACT_APP_BASE_URL}/api`,
+    // baseUrl: `${process.env.REACT_APP_BASE_URL}/api`,
   }),
   tagTypes: ["Suggestions", "Comments", "Planned", "In-progress", "Live"],
   endpoints: (builder) => ({
