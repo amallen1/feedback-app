80e1379416f573f27ed470177ade6f93436f44b7
amallen1 <amallen046@gmail.com>
2025-02-14 16:54:52 -0600

    clean up and minor edits to package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-//I updated styled components and material UI to the current latest version
-//I don't see any breaking changes as of yet.
-//I got rid of the styledcomponents/macro file since it's outdated in v6 styled component
-//I figured out how to add the correct babel plugin below
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
